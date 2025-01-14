22fe00ca68e2acb428533d21ea9e4085d4bb0c18
crispipear <lisuy97@gmail.com>
2025-01-14 01:41:02 -0800

    remove unused import to fix ts error, ugh

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -1,6 +1,6 @@
 import { Component, h } from '@stencil/core';
 import { Router } from '../../';
-import { Route, match } from 'stencil-router-v2';
+import { Route } from 'stencil-router-v2';
 import { TextColor, TextSize, TextVariant } from '../../common/namespaces';
 import { fetchApiData } from '../../common/api';
 import state from '../../store';
