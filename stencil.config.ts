4c72b715182a80d9ac0f5ddb89252d6350047ee1
crispipear <lisuy97@gmail.com>
2025-01-20 23:49:28 -0800

    remove dotenv and add index redirect to deploy config

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,23 +1,14 @@
 import { Config } from '@stencil/core';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
 
 export const config: Config = {
   globalStyle: 'src/global/app.css',
   globalScript: 'src/global/app.ts',
   taskQueue: 'async',
-  env: {
-    PRISMIC_ENTRY_POINT: process.env.PRISMIC_ENTRY_POINT,
-    PRISMIC_ACCESS_TOKEN: process.env.PRISMIC_ACCESS_TOKEN,
-    MS_CHAR_NAME: process.env.MS_CHAR_NAME,
-  },
   outputTargets: [
     {
       type: 'www',
-      // comment the following line to disable service workers in production
       serviceWorker: null,
-      baseUrl: 'https://myapp.local/',
+      baseUrl: '/',
     },
   ]
 };
